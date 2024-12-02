657b4fe115f31dc134fe1df42abd19e6118fa646
Ricardo Piro-Rael <fdisk@fdisk.co>
2024-12-02 02:34:01 -0800

    Improve mobile view

--- src/components/case_studies/gaintain/mockups.tsx
+++ src/components/case_studies/gaintain/mockups.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import MockupsImage from 'images/gaintain/mockups.webp'
 
 const Mockups = (): JSX.Element => (
-  <div data-aos='fade-up' className='gaintain-mockups' style={{ background: `url(${MockupsImage})` }} />
+  <div data-aos='fade-up' className='gaintain-mockups' style={{ backgroundImage: `url(${MockupsImage})` }} />
 )
 
 export default Mockups
