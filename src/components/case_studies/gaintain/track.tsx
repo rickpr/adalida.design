72b4cabf5463055291626aef3c04e14c46d8bb5f
Ricardo Piro-Rael <fdisk@fdisk.co>
2025-01-13 20:41:20 -0800

    Remove extra gap

--- src/components/case_studies/gaintain/track.tsx
+++ src/components/case_studies/gaintain/track.tsx
@@ -4,7 +4,7 @@ import TrackImage from 'images/gaintain/track.webp'
 
 const Track = (): JSX.Element => {
   return (
-    <div data-aos='fade-up' className='gaintain-top-to-bottom'>
+    <div data-aos='fade-up' className='gaintain-about'>
       <div className='gaintain-header'>GAINTAIN</div>
       <h1 className='default-weight'>Where Gains are Data-Driven</h1>
       <img src={TrackImage} alt='Where Gains are Data-Driven' />
