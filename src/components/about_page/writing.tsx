1364ab95b7bf73e15e08e990e48653022fffb55a
Ricardo Piro-Rael <fdisk@fdisk.co>
2025-02-02 18:50:36 -0800

    Remove writings that break the layout

--- src/components/about_page/writing.tsx
+++ src/components/about_page/writing.tsx
@@ -3,11 +3,6 @@ import React from 'react'
 import Section from './section'
 
 const writings = [
-  { title: 'Affordances + Signifiers', date: 'Dec ‘24' },
-  { title: 'Visualizing the Paradox of User Experience Design', date: 'Dec ‘24' },
-  { title: 'How I planned my Elopement with Notion', date: 'Dec ‘24' },
-  { title: 'Leveraging the Fitch Style Proof In Design', date: 'Dec ‘24' },
-  { title: 'Journey Mapping with Nielson, Norman, and Wittgenstein', date: 'Dec ‘24' },
   { title: 'User Research and The Explanatory Gap', date: 'Dec ‘24' },
   { title: 'B.A of English - Philosophy | Work', date: 'Dec ‘24' },
   { title: 'Helpful Quotes', date: 'Dec ‘24' },
