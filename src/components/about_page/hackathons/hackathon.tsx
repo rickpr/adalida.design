7bee6a4729fb73f6951f54c45a947cadd2cf47e1
Ricardo Piro-Rael <fdisk@fdisk.co>
2025-03-19 00:51:33 -0700

    Improve card style

--- src/components/about_page/hackathons/hackathon.tsx
+++ src/components/about_page/hackathons/hackathon.tsx
@@ -7,13 +7,15 @@ import type { Hackathon as HackathonType } from 'hackathons'
 const Hackathon = ({ hackathon }: { hackathon: HackathonType }): JSX.Element => {
   return (
     <div className='glass card hackathon'>
-      <div className='info-row'>
-        <div className='caption-1'>{hackathon.location}</div>
-        <div className='caption-1'>{hackathon.year}</div>
+      <div className='inside'>
+        <div className='info-row'>
+          <div className='caption-1'>{hackathon.location}</div>
+          <div className='caption-1'>{hackathon.year}</div>
+        </div>
+        <div className='subtitle-1'><strong>{hackathon.name}</strong></div>
+        <div className='info-row caption-1'>{hackathon.sponsor}</div>
+        <div className='caption-1'>{hackathon.description}</div>
       </div>
-      <div className='subtitle-1'>{hackathon.name}</div>
-      <div className='info-row caption-1'>{hackathon.sponsor}</div>
-      <div className='caption-1'>{hackathon.description}</div>
       <UniversalLink to={hackathon.link} className='btn animated-link'>
         VIEW PRESENTATION <IconArrowNarrowRight />
       </UniversalLink>
