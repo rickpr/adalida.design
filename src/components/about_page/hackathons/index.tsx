4af88fb80502e1c091e5135138f8931e89182a0f
Ricardo Piro-Rael <fdisk@fdisk.co>
2025-03-19 02:48:28 -0700

    Add hackathon titles

--- src/components/about_page/hackathons/index.tsx
+++ src/components/about_page/hackathons/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import HackathonsList from 'hackathons'
 
+import Section from '../section'
 import Hackathon from './hackathon'
 
 // TODO: Handle more than 5 hackathons
@@ -9,14 +10,17 @@ const firstTwoHackathons = HackathonsList.slice(0, 2)
 const remainingHackathons = HackathonsList.slice(2)
 
 const Hackathons = (): React.ReactElement => (
-  <>
+  <Section title='Hackathons'>
+    <div className='body-2'>
+      I enjoy solving complex problems in fast-paced, 1-2 day hackathons. Check out my past submissions:
+    </div>
     <div className='hackathons odd'>
       {firstTwoHackathons.map((hackathon) => <Hackathon key={hackathon.name} hackathon={hackathon} />)}
     </div>
     <div className='hackathons even'>
       {remainingHackathons.map((hackathon) => <Hackathon key={hackathon.name} hackathon={hackathon} />)}
     </div>
-  </>
+  </Section>
 )
 
 export default Hackathons
